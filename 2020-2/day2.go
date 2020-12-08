ab4cf4a1f20a44332cb015678069f9e07367da0a
harrison_chen <harrison_chen@trendmicro.com>
2020-12-08 13:28:35 +0800

    update 2020 day2

--- 2020-2/day2.go
+++ 2020-2/day2.go
@@ -2,35 +2,35 @@ package main
 
 import (
 	"bufio"
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
 func main() {
 	// Save input from https://adventofcode.com/2020/day/1/input, data content might be different from users
-	file, err := os.Open("input2.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
 	displayCountRules := make([]string, 0)
 	displayChar := make([]string, 0)
 	targetPasswords := make([]string, 0)
 
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
 		s := strings.Fields(scanner.Text())
 		displayCountRules = append(displayCountRules, s[0])
 		displayChar = append(displayChar, s[1])
 		targetPasswords = append(targetPasswords, s[2])
-    }
+	}
 
-    if err := scanner.Err(); err != nil {
-        log.Fatal(err)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 
 	// 2020-1 advent code is here "https://adventofcode.com/2020/day/2"
@@ -38,9 +38,9 @@ func main() {
 	fmt.Println("Anser for problem 2: ", problem2(displayCountRules, displayChar, targetPasswords))
 }
 
-func problem1 (displayCountRules, displayChar, targetPasswords []string) int {
+func problem1(displayCountRules, displayChar, targetPasswords []string) int {
 	validCount := 0
-	for i:=0; i<len(displayCountRules); i++ {
+	for i := 0; i < len(displayCountRules); i++ {
 		sizeSpl := strings.Split(displayCountRules[i], "-")
 		min, _ := strconv.Atoi(sizeSpl[0])
 		max, _ := strconv.Atoi(sizeSpl[1])
@@ -52,21 +52,20 @@ func problem1 (displayCountRules, displayChar, targetPasswords []string) int {
 	return validCount
 }
 
-func problem2 (displayCountRules, displayChar, targetPasswords []string) int {
+func problem2(displayCountRules, displayChar, targetPasswords []string) int {
 	validCount := 0
-	for i:=0; i<len(displayCountRules); i++ {
+	for i := 0; i < len(displayCountRules); i++ {
 		sizeSpl := strings.Split(displayCountRules[i], "-")
 		min, _ := strconv.Atoi(sizeSpl[0])
 		max, _ := strconv.Atoi(sizeSpl[1])
 		if string(targetPasswords[i][min-1]) == string(displayChar[i][0]) && string(targetPasswords[i][max-1]) != string(displayChar[i][0]) {
-			if i == len(displayCountRules)-1 {
-				fmt.Println(targetPasswords[i], min, string(targetPasswords[i][min-1]), max, string(targetPasswords[i][max-1]), string(displayChar[i][0]))
-			}
+			validCount++
+		}
+
+		if string(targetPasswords[i][min-1]) != string(displayChar[i][0]) && string(targetPasswords[i][max-1]) == string(displayChar[i][0]) {
 			validCount++
 		}
 	}
 
 	return validCount
 }
-
-
